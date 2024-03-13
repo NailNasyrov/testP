fb23065a32926ac79646d4cc187f7d8ec3d6f4c1
Nail Nasyrov <nail_nasyrov@mail.ru>
2024-03-13 10:51:32 +0300

    last version

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,138 @@
+import java.util.Scanner;
 public class Main {
+    public enum TypeOfOperand {
+        notANumber,
+        Arabian,
+        Roman
+    }
+
+    public static String[] numbersA = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    public static String[] numbersR = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    public static String[] numbersR2 = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX"};
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("test");
+        String row;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input:");
+        row = in.nextLine();
+
+        System.out.println(Calc(row));
+    }
+
+    public static String Calc(String row) {
+        row = row.replace(" ", "");
+        //разделим строку
+        String[] items = row.split("[+\\-*/]", 2);
+
+        try {
+            //В выражении точно два операнда и одна операция
+            if (items.length != 2) {
+                throw new Exception("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            }
+
+            String num1 = items[0].trim();
+            String num2 = items[1].trim();
+
+            //операнды являются числами, попадают в диапазон
+            //-1 это не число, 1 - это арабское число, 2 - это римское число
+            TypeOfOperand type_num1 = CorrectOperand(num1);
+            TypeOfOperand type_num2 = CorrectOperand(num2);
+
+            try {
+                //оба операнда числа и одной системы счисления
+                if (type_num1 != type_num2) {
+                    throw new Exception("Используются одновременно разные системы счисления");
+                }
+
+                if (type_num1 == TypeOfOperand.notANumber) {
+                    throw new Exception("Строка не является математической операцией.");
+                }
+                //Вычисляем. Передаем строку, тип числа type_num1 или type_num2, сами числа
+                return Calculate(row, type_num1, num1, num2);
+
+            } catch (Exception ex) {
+                return ex.getMessage();
+            }
+
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public static TypeOfOperand CorrectOperand(String operand) {
+        //определяем, что число арабское
+        for (String i : numbersA) {
+            if (i.equals(operand)) {
+                return TypeOfOperand.Arabian;
+            }
+        }
+        //иначе определяем, что число римское
+        for (String i : numbersR) {
+            if (i.equals(operand)) {
+                return TypeOfOperand.Roman;
+            }
+        }
+        //это не число
+        return TypeOfOperand.notANumber;
+    }
+
+    public static String Calculate(String row, TypeOfOperand type, String a, String b) {
+        int res = 0;
+        //это римские числа, то преобразуем в арабские
+        if (type == TypeOfOperand.Roman) {
+            a = ConvertRToA(a);
+            b = ConvertRToA(b);
+        }
+        //вычисляем
+        char[] charRow = row.toCharArray();
+        for (char c : charRow) {
+            res = switch (c) {
+                case '+' -> Integer.parseInt(a) + Integer.parseInt(b);
+                case '-' -> Integer.parseInt(a) - Integer.parseInt(b);
+                case '/' -> Integer.parseInt(a) / Integer.parseInt(b);
+                case '*' -> Integer.parseInt(a) * Integer.parseInt(b);
+                default -> res;
+            };
+        }
+        //это римские числа, то результат преобразуем в римское
+        if (type == TypeOfOperand.Roman) {
+            try {
+                if (res <= 0) {
+                    throw new Exception("В римской системе нет отрицательных чисел");
+                }
+                return ConvertAToR(res);
+            } catch (Exception ex) {
+                return ex.getMessage();
+            }
+        }
+        //это арабские числа, результат арабское число
+        return Integer.toString(res);
+    }
+
+    public static String ConvertRToA(String r) {
+        String res = "";
+        //ищем соответсвие римского числа арабскому
+        for (int i = 0; i < numbersR.length; ++i) {
+            if (r.equals(numbersR[i])) {
+                //нашли, прервем цикл
+                res = Integer.toString(i + 1);
+                break;
+            }
+        }
+        return res;
+    }
+
+    public static String ConvertAToR(int a) {
+        String res = "";
+
+        for (int i = 0; i < numbersR2.length; ++i) {
+            if ((i + 1) == a) {
+                //нашли, прервем цикл
+                res = numbersR2[i];
+                break;
+            }
+        }
+        return res;
     }
 }
